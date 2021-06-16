853cd90e2a810005f72a5515c59535cf97e7983b
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-06-15 23:38:20 -0700

    Add routes with home page redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SketchComponent } from './sketch/sketch.component';
+import { AsteroidsComponent } from './asteroids/asteroids.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sketch', pathMatch: 'full', component: SketchComponent },
+  { path: 'asteroids', pathMatch: 'full', component: AsteroidsComponent },
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
