d5e1346c150e57fb8922d2ff9053d4b0220b0ca0
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-06-15 23:42:08 -0700

    Add sketch and asteroids components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SketchComponent } from './sketch/sketch.component';
+import { AsteroidsComponent } from './asteroids/asteroids.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SketchComponent,
+    AsteroidsComponent
   ],
   imports: [
     BrowserModule,
