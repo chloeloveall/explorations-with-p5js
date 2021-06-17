0e068d09e91fe95bc7a8b2d8b2442dfe3a3822af
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-06-16 20:28:57 -0700

    Add pagenotfound and home components to declarartions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SketchComponent } from './sketch/sketch.component';
 import { AsteroidsComponent } from './asteroids/asteroids.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SketchComponent,
-    AsteroidsComponent
+    AsteroidsComponent,
+    PageNotFoundComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
