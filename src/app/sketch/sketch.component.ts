b98815d738f71d1189ba86097818978423759427
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-06-16 20:25:35 -0700

    Update sketch component with parent

--- src/app/sketch/sketch.component.ts
+++ src/app/sketch/sketch.component.ts
@@ -11,17 +11,26 @@ export class SketchComponent implements OnInit {
     const sketch = (p5: any) => {
       let penSize = 1;
       let penState = 0;
+      let mode = 0;
+      var cnv = p5;
       
       p5.setup = () => {
+        // resetCanvas(p5)
         // let cnv = p5.createCanvas(p5.windowWidth - 100, 500);
-        let cnv = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+        cnv = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+        cnv.parent('sketchContainer')
         // var x = (p5.windowWidth - p5.width) / 2;
         // var y = (p5.windowHeight - p5.height) / 1;
         // cnv.position(x, y)
-        // p5.background(255)
+        p5.background(255)
       }
 
       p5.draw = () => {
+        // var canvasDiv = document.getElementById('sketch-container')
+        // var width = canvasDiv?.offsetWidth
+        // var height = canvasDiv?.offsetHeight
+        // p5.resizeCanvas(width, height)
+        
         if (p5.mouseIsPressed) {
           if (penState == 0) {
             p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
@@ -65,13 +74,17 @@ export class SketchComponent implements OnInit {
         if (p5.key == 'b') {
           p5.stroke(0, 0, 255);
         }
+
+        if(p5.key == 'e'){
+          penState = 3;
+        }
         
         if (p5.key == 'x') {
           // x pen
           penState = 2;
         }
         
-        if (p5.key == 'e') {
+        if (p5.key == 'k') {
           // circle pen
           penState = 1;
         }
@@ -80,6 +93,15 @@ export class SketchComponent implements OnInit {
           // line pen
           penState = 0;
         }
+
+        if (p5.key == 'p') {
+          // saves canvas as a png
+          p5.saveCanvas('myCanvas', 'png')
+        }
+
+        // if (p5.key == 'u') {
+        //   cnv.hide()
+        // }
       }
 
       p5.keyPressed = () => {
@@ -96,7 +118,12 @@ export class SketchComponent implements OnInit {
 
       p5.windowResized = () => {
         p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+        p5.background(255)
       }
+
+      // p5.mousePressed = () => {
+      //   p5.remove(); // remove whole sketch on mouse press
+      // }
     }
     var canvas = new p5(sketch);
   }
