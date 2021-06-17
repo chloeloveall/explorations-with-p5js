3fdc98b2cdccaab4c0d97b5e34b1702f4420cad7
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-06-16 20:29:35 -0700

    Update routing to include home and pagenotfound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SketchComponent } from './sketch/sketch.component';
 import { AsteroidsComponent } from './asteroids/asteroids.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'sketch', pathMatch: 'full', component: SketchComponent },
-  { path: 'asteroids', pathMatch: 'full', component: AsteroidsComponent },
-  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  { path: 'sketch', component: SketchComponent },
+  { path: 'asteroids', component: AsteroidsComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
 
 @NgModule({
