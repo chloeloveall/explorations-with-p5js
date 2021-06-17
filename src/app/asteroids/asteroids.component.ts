6902b4875427f26a8f8f7d66d726b776fddce1a5
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-06-16 20:34:45 -0700

    Add p5 setup, draw, keyReleased, and keyPressed

--- src/app/asteroids/asteroids.component.ts
+++ src/app/asteroids/asteroids.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Ship from './js/ship';
+import Asteroid from './js/asteroid';
+import * as p5 from 'p5';
 
 @Component({
   selector: 'arcade-asteroids',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asteroids.component.scss']
 })
 export class AsteroidsComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit(): void {
-  }
+    
+    const asteroids = (p5: any) => { 
+      let ship: Ship;
+      var cnv = p5;
 
+      p5.setup = () => {
+        cnv = p5.createCanvas(p5.windowWidth, p5.windowHeight)
+        cnv.parent('asteroidsContainer')
+        ship = new Ship(p5)
+      }
+
+      p5.draw = () => {
+        p5.background(0)
+        // p5.translate(ship.pos.x, ship.pos.y)
+        ship.render()
+        ship.turn()
+        ship.update()
+        ship.edges()
+
+        for (let i = 0; i < asteroids.length; i++) {
+
+        }
+      }
+
+      p5.keyReleased = () => {
+        ship.setRotation(0)
+        ship.boosting(false)
+      }
+
+      p5.keyPressed = () => {
+        if (p5.keyCode == p5.RIGHT_ARROW) {
+          ship.setRotation(0.1)
+        } else if (p5.keyCode == p5.LEFT_ARROW) {
+          ship.setRotation(-0.1)
+        } 
+        else if (p5.keyCode == p5.UP_ARROW) {
+          ship.boosting(true)
+        }
+      }
+    }
+  var canvas = new p5(asteroids);
+  }
 }
